faa19f561190e66c2e768655100cfb4d02cf742f
Faiz <faiz.anwar4749@gmail.com>
2020-11-16 14:01:51 +0530

    [Faiz] Add: Displaying Welcome message

---
+++ ProductReviewManagementProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProductReviewManagementProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Program.");
+        }
+    }
+}
